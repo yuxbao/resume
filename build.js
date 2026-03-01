1197fffb333751c701b3aa1e3e595603ab572446
Ethan Bao <522425561@qq.com>
2026-03-01 15:05:30 +0800

    feat

--- build.js
+++ build.js
@@ -1,60 +1,62 @@
-const fs = require('fs-extra')
-const axios = require('axios')
-const puppeteer = require('puppeteer')
+const fs = require("fs-extra");
+const axios = require("axios");
+const puppeteer = require("puppeteer");
 
-const gist = 'antfu/ceb04ede6daf195eaf51e32b6aef5d4e'
+const gist = "yuxbao/50e74b48f2bc188fe549a2aef7ba82ce";
 
 async function buildHTML() {
-  await fs.remove('./dist')
-  await fs.ensureDir('./dist')
+  await fs.remove("./dist");
+  await fs.ensureDir("./dist");
 
-  let resume
-  if (fs.existsSync('./resume.json')) {
-    console.log(`Loading from locale "resume.json"`)
-    resume = JSON.parse(fs.readFileSync('./resume.json', 'utf-8'))
+  let resume;
+  if (fs.existsSync("./resume.json")) {
+    console.log(`Loading from locale "resume.json"`);
+    resume = JSON.parse(fs.readFileSync("./resume.json", "utf-8"));
   } else {
-    console.log(`Downloading resume... [${gist}]`)
-    const { data } = await axios.get(`https://gist.githubusercontent.com/${gist}/raw/resume.json`)
-    resume = data
+    console.log(`Downloading resume... [${gist}]`);
+    const { data } = await axios.get(
+      `https://gist.githubusercontent.com/${gist}/raw/resume.json`,
+    );
+    resume = data;
   }
-  console.log('Rendering...')
-  const html = await require("./index.js").render(resume)
-  console.log('Saving file...')
-  fs.writeFileSync('./dist/index.html', html, 'utf-8')
-  console.log('Done')
-  return html
+  console.log("Rendering...");
+  const html = await require("./index.js").render(resume);
+  console.log("Saving file...");
+  fs.writeFileSync("./dist/index.html", html, "utf-8");
+  console.log("Done");
+  return html;
 }
 
 async function buildPDF(html) {
-  const browser = await puppeteer.launch({ headless: true })
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  console.log('Opening puppeteer...')
-  await page.setContent(html, { waitUntil: 'networkidle0' })
-  console.log('Generating PDF...')
+  console.log("Opening puppeteer...");
+  await page.setContent(html, { waitUntil: "networkidle0" });
+  console.log("Generating PDF...");
   const pdf = await page.pdf({
-    format: 'A4', 
-    displayHeaderFooter: false, 
+    format: "A4",
+    displayHeaderFooter: false,
     printBackground: true,
     margin: {
-      top: '0.4in',
-      bottom: '0.4in',
-      left: '0.4in',
-      right: '0.4in',
-    }
-  })
-  await browser.close()
-  console.log('Saving file...')
-  fs.writeFileSync('./dist/resume.pdf', pdf)
-  console.log('Done')
-  return pdf
+      top: "0.4in",
+      bottom: "0.4in",
+      left: "0.4in",
+      right: "0.4in",
+    },
+  });
+  await browser.close();
+  console.log("Saving file...");
+  fs.writeFileSync("./dist/resume.pdf", pdf);
+  console.log("Done");
+  return pdf;
 }
 
 async function buildAll() {
-  const html = await buildHTML()
-  await buildPDF(html)
+  const html = await buildHTML();
+  await buildPDF(html);
 }
 
-buildAll().catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+buildAll().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
